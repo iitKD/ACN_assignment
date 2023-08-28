234570bc95e6a70e2f4388c5d86c7e3a56b7c6ef
iitKD <142815479+iitKD@users.noreply.github.com>
2023-08-28 10:10:59 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ set -e
 # Note: Mininet must be run as root.  So invoke this shell script
 # using sudo.
 
-time=60
+time=100
 bwnet=10
 # TODO: If you want the RTT to be 4ms what should the delay on each
 # link be?  Set this value correctly.
-delay=1
+delay=1.5
 
 iperf_port=5001
 
